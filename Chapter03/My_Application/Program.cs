378a622e2c49304c6744f6b1cafaff20b711ebed
Peter Berry <pwcberry@gmail.com>
2026-02-27 10:33:58 +1100

    [Chapter 03]: Done

--- Chapter03/My_Application/Program.cs
+++ Chapter03/My_Application/Program.cs
@@ -1,16 +1,20 @@
+using Microsoft.AspNetCore.HttpLogging;
 using MyApplication;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(opts => opts.LoggingFields = HttpLoggingFields.RequestProperties);
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
+
 app.MapGet("/", () => "Hello world!");
-
 app.MapGet("/sum", (int a, int b) => $"{a} + {b} = {(a + b)}");
-
-app.MapGet("/person", (string name, int age) => new Person(name, age).ToString());
-
+app.MapGet("/person", (string name, int age) => new Person(name, age));
 app.MapPost("/person", (Person p) => p.ToString());
 
 app.Run();
-
-
