b7a96c1032fd8c9fc2e0d74d94e359feb4128158
Peter Berry <pwcberry@gmail.com>
2026-03-20 09:53:05 +1100

    Finish Chapter 6

--- My_Learning/Chapter06/Program.cs
+++ My_Learning/Chapter06/Program.cs
@@ -11,7 +11,27 @@ var app = builder.Build();
 
 app.MapGet("/", () => Results.Ok("OK"));
 
-
+app.MapGet("/employees", (NorthwindService service) => service.GetEmployees());
+app.MapGet("/employee/{id:int}", (NorthwindService service, int id) => service.GetEmployee(id));
 app.MapGet("/categories", (NorthwindService service) => service.GetCategories());
+app.MapGet("/territories/{name}/orders", (NorthwindService service, string name) =>
+{
+    var territory = service.GetTerritoryByName(name);
+    if (territory is null)
+        return Results.NotFound();
+
+    var orders = service.GetOrdersByTerritory(territory.Id);
+    return Results.Ok(orders);
+}).WithName("orders-by-territories");
+
+app.MapGet("/redirect", (LinkGenerator linkGen) =>
+{
+    // If the mapped route above isn't given an Endpoint name (using the method "WithName")
+    // then the link generator will return null as it's not able to find the route by name.
+    var url = linkGen.GetPathByName("orders-by-territories",  new { name = "new york" });
+    return Results.Redirect(url!);
+});
+
+app.MapGet("/territory/{name}", (string name) => Results.RedirectToRoute("orders-by-territories", new { name }, permanent: true, preserveMethod: true));
 
 app.Run();
--- My_Learning/Chapter06/Services/NorthwindService.cs
+++ My_Learning/Chapter06/Services/NorthwindService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
 using MyLearning.Data.Sqlite;
 using MyLearning.Data.Sqlite.Northwind;
 
@@ -7,5 +8,29 @@ namespace MyLearning.Chapter06.Services
     public class NorthwindService(NorthwindContext context)
     {
         public IList<Category> GetCategories() => context.Categories.ToList();
+
+        public IList<Employee> GetEmployees() => context.Employees.ToList();
+
+        public Employee? GetEmployee(int id) => context.Employees.Find(id);
+
+        public Territory? GetTerritoryByName(string name) => context.Territories.FirstOrDefault(t => t.TerritoryDescription!.ToLower() == name.ToLower());
+
+        public IList<OrderSummary> GetOrdersByTerritory(string territoryId) {
+            var query = from o in context.Orders
+                        join e in context.Employees on o.EmployeeId equals e.Id
+                        join et in context.EmployeeTerritories on e.Id equals et.EmployeeId
+                        join t in context.Territories on et.TerritoryId equals t.Id
+                        where t.Id == territoryId
+                        select new OrderSummary(
+                            OrderId: o.Id,
+                            EmployeeId: o.EmployeeId,
+                            ShippedDate: o.ShippedDate,
+                            Territory: t.TerritoryDescription
+                        );
+
+            return query.ToList();
+        }
     }
+
+public record OrderSummary(int OrderId, int EmployeeId, DateOnly? ShippedDate, string? Territory);
 }
